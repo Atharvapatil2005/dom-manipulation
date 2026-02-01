09918674382cab0e014aa92dd2bb2b5acafa4e61
Atharva Patil <atharvaspatil2005@gmail.com>
2026-02-01 16:06:57 +0530

    defined schema for database

--- backend/validation.js
+++ backend/validation.js
@@ -1,5 +1,11 @@
  const zod=require("zod");
  app.use(zod.json());
- do {
-    
- } while (condition);
+
+ const createTodo=zod.objects({
+    title:zod.string(),
+    description:zod.string()
+ })
+
+ const updateTodo=zod.objects({
+    id:zod.string(),
+ })
