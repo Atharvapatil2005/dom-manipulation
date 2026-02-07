9f1dc5cf4bd604e5511c5fd4aa0c438a74e61611
Atharva Patil <atharvaspatil2005@gmail.com>
2026-02-07 08:37:11 +0530

    added style

--- backend/server.js
+++ backend/server.js
@@ -9,31 +9,32 @@ const {todo}=require("./database")
 
 
 app.post("/todo",async function(req,res) {
-    const createPayload=req.body;
-    const parsedPayload=createTodo.safeParse(createPayload);
-    if(!parsedPayload.success){
-        res.status(411).json({
+    const createPayload=req.body;       //we are storing the data sent via body in a constant(createPayload)
+    const parsedPayload=createTodo.safeParse(createPayload);//here we are validating the input using Zod(createTodo)
+                                    // safeParse dosen't the app to crash and handles worng inputs 
+    if(!parsedPayload.success){         //if the inputs sent are not validated 
+        res.status(411).json({          //responding with status and json message
             msg:"you sent wrong inputs"
         })
         return;
 
     }
-    //feeding into db
-    await todo.create({
-        title:createPayload.title,
+    //if the inputs pass then feeding into db
+    await todo.create({     
+        title:createPayload.title,      //createPayload the sent input stored in the DB
         description:createPayload.description,
         completed: false
     })
-    res.json({
+    res.json({          //responding with json
         msg:"todo created"
     })
     
 })
 
 
-app.get("/todos", async function (req,res) {
-    const todos= await todo.find({});
-        res.json({
+app.get("/todos", async function (req,res) {    
+    const todos= await todo.find({});// returns all the documents without any filter
+        res.json({          //we are sending a json response
             todos: todos
         })
     
@@ -41,7 +42,7 @@ app.get("/todos", async function (req,res) {
 
 app.put("/completed",async function (req,res) {
     const updatePayload=req.body;
-    const parsedPayload=updateTodo.safeParse(updatePayload);
+    const parsedPayload=updateTodo.safeParse(updatePayload);        //validating via zod updateTodo
     if(!parsedPayload.success){
         res.status(411).json({
             msg:"you sent the wrong inputs",
@@ -49,9 +50,9 @@ app.put("/completed",async function (req,res) {
         return;
     }
     await todo({
-        _id:req.body.id
+        _id:req.body.id  //this expects the user id via postman
     },{
-        completed:true
+        completed:true          
     })
     res.json({
         msg:"todo marked as completed"
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { CreateTodo } from "./components/CreateTodo";
-import { Todos } from "./components/Todos";
+import { Todos } from "./components/todos";
 
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]); {/* todos is the cuerent state and setTodos is used to update it */}
 
   useEffect(() => {
-    fetch("http://localhost:3000/todos")
+    fetch("http://localhost:3000/todos")  
       .then(async (res) => {
         const json = await res.json();
         setTodos(json.todos);
